d0f6b03f43da8ad9043a162b09e5e3b9a7448d72
thon1525 <git@github.com>
2024-01-24 05:29:52 +0700

    add this crudbooster

---
+++ CRUDBooster/crudbooster-sample-master/crudbooster-sample-master/vendor/crocodicstudio/crudbooster/src/controllers/hellouserController.php
@@ -0,0 +1,24 @@
+<?php namespace crocodicstudio\crudbooster\controllers;
+
+use crocodicstudio\crudbooster\controllers\Controller;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\PDF;
+use Illuminate\Support\Facades\Excel;
+use CRUDBooster;
+
+class hellouserController extends CBController {
+
+public function hellothon(){
+    return view('crudbooster::hellothon');
+}
+
+}
